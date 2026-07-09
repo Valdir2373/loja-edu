a5459e0ca1058c1a38a433a2f9b0323a1535c65c
Valdir2373 <valdirdesouzajunioradm@gmail.com>
2026-07-08 23:31:12 -0300

    criação do modulo product

--- src/infra/module/ProductModule.ts
+++ src/infra/module/ProductModule.ts
@@ -1,12 +1,18 @@
 import { DataAccessPort } from "../../domain/database/DataAcess";
+import { Product } from "../../domain/entites/Product";
+import { RepositoryPort } from "../../domain/repository/RepositoryPort";
 import { DependencyInjection } from "../pattern/DI";
+import { ProductRepository } from "../repository/ProductRepository";
 import { ServerPort } from "../server/ServerPort";
 
 export class ProductModule {
     private server:ServerPort
     private db:DataAccessPort
+    private productRepository:RepositoryPort<Product>
     constructor(private di:DependencyInjection) {
         this.db = this.di.getDependency(DataAccessPort)
+        this.productRepository = new ProductRepository(this.db)
         this.server = this.di.getDependency(ServerPort)
+        
     }
 }
